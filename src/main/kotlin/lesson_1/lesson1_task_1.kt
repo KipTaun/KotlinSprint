a988c567f6cafbfef5ebc7589126e1b092b7e31c
KipTaun <shirokov2719@gmail.com>
2023-05-07 14:57:26 +0300

    Исравил форматирование согласно конвенции.

--- src/main/kotlin/lesson_1/lesson1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson1_task_1.kt
@@ -1,6 +1,5 @@
 package lesson_1
-fun main()
-{
-    var quantityOfOrders:Byte=75
-    val textWithThanks:String="Спасибо за покупку. Ждем Вас ещё"
+fun main() {
+    var quantityOfOrders: Byte = 75
+    val textWithThanks: String = "Спасибо за покупку. Ждем Вас ещё"
 }
