a1dd197a794382f40002aee3c75f89fb9313cf25
KipTaun <shirokov2719@gmail.com>
2023-05-07 15:30:00 +0300

    Initial Commit

---
+++ src/main/kotlin/lesson1_task_3.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println("0$hour")
+    println("0$minute")
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute" )
+}
