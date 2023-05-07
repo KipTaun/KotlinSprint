dce79e6e1b8a7e24c58ce4956166e17d4c940575
KipTaun <shirokov2719@gmail.com>
2023-05-07 15:03:26 +0300

    префикс задачи?(можно пример)
    Исравил форматирование согласно конвенции.

--- src/main/kotlin/lesson_1/lesson1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson1_task_2.kt
@@ -1,14 +1,15 @@
 package lesson_1
 
-fun  main()
-{
+fun  main(){
+    var quantityOfOrders: Byte = 75
+    var quantityOfWorkers: Int = 2000
+    val textWithThanks: String = "Спасибо за покупку. Ждем Вас ещё"
 
-    var quantityOfOrders:Byte=75
-    var quantityOfWorkers:Int=2000
-    val textWithThanks:String="Спасибо за покупку. Ждем Вас ещё"
     println(quantityOfOrders)
     println(textWithThanks)
     // println(quantityOfWorkers)
-    quantityOfWorkers=1999
+
+    quantityOfWorkers = 1999
+
     println(quantityOfWorkers)
 }
