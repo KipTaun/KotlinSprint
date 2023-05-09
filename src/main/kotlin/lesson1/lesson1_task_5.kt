6beed8f4ceb067bc01f3a4117afc5df9a44157f7
KipTaun <shirokov2719@gmail.com>
2023-05-09 21:59:05 +0300

    знак деления  окружил пробелами). а есть какой небуть автомотичекский способ?

--- src/main/kotlin/lesson1/lesson1_task_5.kt
+++ src/main/kotlin/lesson1/lesson1_task_5.kt
@@ -2,7 +2,7 @@ package lesson1
 
 fun main() {
   val totalSecond = 6480
-  val minute = totalSecond/60
+  val minute = totalSecond / 60
 
   println(String.format("%d:%02d", minute, totalSecond % 60))
 }
