f5e59fe963deca9850420c0d49a4d9d4a3c9a3ff
KipTaun <shirokov2719@gmail.com>
2023-05-09 16:33:45 +0300

    использовал сразу все переменные в отформатированной строке)

--- src/main/kotlin/lesson1/lesson1_task_5.kt
+++ src/main/kotlin/lesson1/lesson1_task_5.kt
@@ -1,11 +1,8 @@
 package lesson1
 
-fun main(args: Array<String>) {
+fun main() {
   val totalSecond = 6480
   val minute = totalSecond/60
 
-  val formattedMinute = String.format("%02d", minute)
-  val formattedSecond = String.format("%02d", totalSecond % 60)
-
-  println("$formattedMinute:$formattedSecond")
+  println(String.format("%d:%02d", minute, totalSecond % 60))
 }
